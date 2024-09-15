7f8743e005827319a6620fb23aca6c4e0f3aa9bf
ssadown <bibatututu@yandex.ru>
2024-09-15 15:04:28 +0500

    Task5

---
+++ Tyuiu.MiliukovLO.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.MiliukovLO.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.MiliukovLO.Sprint0.Task5.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = " + DataService.Addition(1, 5));
+            Console.WriteLine("A - B = " + DataService.Subtraction(7, 5));
+            Console.WriteLine("A * B = " + DataService.Multiplication(1, 5));
+            Console.WriteLine("A / B = " + DataService.Division(10, 0));
+            Console.ReadKey();
+        }
+    }
+}
