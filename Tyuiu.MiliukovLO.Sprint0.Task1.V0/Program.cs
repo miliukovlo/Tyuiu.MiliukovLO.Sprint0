ac3cbfc7b3494efef6ecaa6b08a879270b9229ef
ssadown <bibatututu@yandex.ru>
2024-09-15 09:39:48 +0500

    Task1

--- Tyuiu.MiliukovLO.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.MiliukovLO.Sprint0.Task1.V0/Program.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.MiliukovLo.Sprint0.Task1.V0
             Console.WriteLine("Hello, World");
             string name = "Игорь";
             int age = 45;
-            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+            Console.WriteLine("Здравствуйте меня зовут "+name+" мне "+age+ " лет!")
             Console.ReadKey();
         }
     }
